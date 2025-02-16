5b5f4c8c1fc9146b7d107c25982a2c29f7c95f14
nimpy-wth <nimmyliala@gmail.com>
2025-02-16 12:23:33 +0700

    random select image

--- test_predictions.py
+++ test_predictions.py
@@ -41,15 +41,32 @@ predicted_classes = np.argmax(predictions, axis=1)
 
 # Show test images with predictions
 num_images = 25  
+random_indices = random.sample(range(len(test_images)), num_images)
+
 plt.figure(figsize=(15, 10))
 
-for i in range(num_images):
-    plt.subplot(5, 5, i+1) 
-    img = test_images[i]
-    true_label = class_labels[true_classes[i]] 
-    predicted_label = class_labels[predicted_classes[i]]
+# for i in range(num_images):
+#     plt.subplot(5, 5, i+1) 
+#     img = test_images[i]
+#     true_label = class_labels[true_classes[i]] 
+#     predicted_label = class_labels[predicted_classes[i]]
     
-    # Convert image from TensorFlow format (0-1 range) back to normal
+#     # Convert image from TensorFlow format (0-1 range) back to normal
+#     img = (img * 255).astype("uint8")
+    
+#     # Display image
+#     plt.imshow(img)
+#     plt.title(f"True: {true_label}\nPred: {predicted_label}", fontsize=12)
+#     plt.axis('off')
+
+# Randomly select 25 images
+for i,idx in enumerate(random_indices):
+    plt.subplot(5, 5, i+1)  # 5 rows, 5 columns
+    img = test_images[idx]  # Get image
+    true_label = class_labels[true_classes[idx]]  # Get true label
+    predicted_label = class_labels[predicted_classes[idx]]  # Get predicted label
+
+        # Convert image from TensorFlow format (0-1 range) back to normal
     img = (img * 255).astype("uint8")
     
     # Display image
